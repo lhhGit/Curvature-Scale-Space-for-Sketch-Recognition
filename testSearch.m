98eefcc7967b6438f2b91a199e41b5437d5b2bc4
lhhGit <adamlhh@gmail.com>
2013-03-03 16:31:00 +0800

    solve defects in algo

--- testSearch.m
+++ testSearch.m
@@ -1,16 +1,10 @@
 directory = 'Dataset';
-results = dir(directory);
-filelist = results(3:end);
-i = 1;
-while(filelist(i).name(1) == '.')
-    i = i+1;
-end
-matrixlist = cell(1,length(filelist)+1 - i);
-filelist = filelist(i:end);
-for j = i:length(filelist)
+filelist = dir(fullfile(directory, '*.mat'));
+
+for j = 1:length(filelist)
    file = filelist(j);
    css_struct = load([directory ,'\\' , file.name],'saved');
-   matrixlist{j-i+1} = css_struct.saved;
+   matrixlist{j} = css_struct.saved;
 end
 
 CSS_I = matrixlist{1};
